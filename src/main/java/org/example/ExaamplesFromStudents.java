b68953b0aadd7d4d9a128abc5c1ac5d8e664c59b
julia <juliiagordin@gmail.com>
2023-10-29 20:58:42 +0300

    add exception lesson part 2

---
+++ src/main/java/org/example/ExaamplesFromStudents.java
@@ -0,0 +1,23 @@
+package org.example;
+
+public class ExaamplesFromStudents {
+    public static void main(String[] args) {
+        // call all 4 functions
+    }
+
+    public static int delimetr(int a, int b) { // todo with try catch
+        return 0;
+    }
+
+    public static int delimetr2(int a, int b) { // todo without any exception
+        return 0;
+    }
+
+    public static void pause(long mill) { // todo with try catch and with finally
+        Thread.sleep(5000);
+    }
+
+    public static void pause2(long mill) { // to do with throws
+        Thread.sleep(5000);
+    }
+}
