63fc63a932861a93a3a8c0e8706d379d6108405f
julia <juliiagordin@gmail.com>
2023-11-01 21:29:29 +0300

    objects part 2

--- src/main/java/datasetup/FillData.java
+++ src/main/java/datasetup/FillData.java
@@ -1,6 +1,8 @@
 package datasetup;
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class FillData {
@@ -12,8 +14,19 @@ public class FillData {
     }
 
     public List<DataCommonDTO> fillData() {
+        UserDto userDto = new UserDto("email@1", 111);
+        CarDTO carDTO1 = new CarDTO("model1", 11111);
+        CarDTO carDTO2 = new CarDTO("model2", 22222);
+        CarDTO[] cars = {carDTO1, carDTO2};
+        DataCommonDTO dataCommonDTO = new DataCommonDTO(userDto, cars);
+        list.add(dataCommonDTO);
 
-        // list.add/.......
+//        list.add(new DataCommonDTO(
+//                new UserDto("emeil2", 333),
+//                Arrays.fill(new CarDTO("model3", 333333), new CarDTO("model4", 44444))
+//        ));
+
+
         return list;
     }
 }
